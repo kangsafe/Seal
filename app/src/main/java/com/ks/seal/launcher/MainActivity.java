aa09c3e9942a3b0b6309cd42861f593a2d553ab5
小康 <kangsafe@163.com>
2018-02-23 13:30:19 +0800

    Initial commit

---
+++ app/src/main/java/com/ks/seal/launcher/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ks.seal.launcher;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView v1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        v1 = (TextView) findViewById(R.id.v1);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/jinqiaofanweibei.ttf");
+        v1.setTypeface(tf);
+    }
+}
